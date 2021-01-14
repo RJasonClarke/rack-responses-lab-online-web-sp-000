8cafe7610120e5fa501fa124bef7edac0e785454
Robert Clarke <r.jason.clarke@gmail.com>
2021-01-14 16:16:31 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,17 @@
-def app
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+  
+    time = Time.now.hour
+  
+    if time <= 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+  
+    resp.finish
+  end
 
 end
-
-puts Time.now
